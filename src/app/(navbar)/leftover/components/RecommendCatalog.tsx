4309cd6af22e75d2aef554a5492f7bfced317b0d
azfaradhi <aradhihakim@gmail.com>
2024-08-25 21:37:50 +0700

    adding leftover page, without database

---
+++ src/app/(navbar)/leftover/components/RecommendCatalog.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RecommendCatalog = () => {
+  return (
+    <div className='container w-5/6 h-72 border-2'>
+
+    </div>
+  )
+}
+
+export default RecommendCatalog
