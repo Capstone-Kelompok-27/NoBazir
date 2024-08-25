50b32ca6ac96f7128841cfd5569bb969620a47ef
Rusmin <rusmn17@gmail.com>
2024-08-25 08:55:36 +0700

    perubahan leftover

--- src/app/(navbar)/leftover/page.tsx
+++ src/app/(navbar)/leftover/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Navbar from "@/app/_components/Navbar";
 
-const page = () => {
+const Page = () => {
+  
+  const isLoggedIn = true;
+
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <Navbar isLoggedIn={isLoggedIn} />
+      <main className="p-4">
+        <h1>LeftOver</h1>
+        <p>This is the leftover page.</p>
+      </main>
+    </div>
+  );
+};
 
-export default page
+export default Page;
