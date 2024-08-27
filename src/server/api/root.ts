4132a8f0b7cfe1ea9e7a4e23acc7c8a9d45882a5
dasarpemrograman <19623209@std.stei.itb.ac.id>
2024-08-27 21:55:35 +0700

    fix: connect communityRouter to the primary Router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 import { authRouter } from "./routers/auth";
 import { catalogRouter } from "./routers/catalog";
+import { communityRouter } from "./routers/community";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { catalogRouter } from "./routers/catalog";
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   catalog: catalogRouter,
+  post: communityRouter,
 });
 
 // export type definition of API
