3b14a594e0b53351b26d8c73cae0f5148c4d6f94
azfaradhi <aradhihakim@gmail.com>
2024-08-28 22:08:36 +0700

    style: editing style

--- src/app/(navbar)/about-us/components/GoalCard.tsx
+++ src/app/(navbar)/about-us/components/GoalCard.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-
+import Image from "next/image";
 interface GoalCardProps {
   link: string;
   des: string;
 }
 
 const GoalCard: React.FC<GoalCardProps> = ({ link, des }) => (
-  <div className="mx-6 flex w-1/2 flex-col rounded-2xl border-2 border-[#679436] bg-[#A5BE00]">
-    <div className="my-2 flex items-center justify-center">
-      <img src={link} alt={des} className="w-24" />
+  <div className="flex flex-col items-center rounded-xl border-2 border-[#679436] bg-[#A5BE00] p-6 shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl">
+    <div className="flex items-center justify-center">
+    <Image
+      src={link}
+      alt={des}
+      width={96}
+      height={96} 
+      className="object-contain"/>    
     </div>
-    <div className="my-1">
+    <div className="mt-4">
       <p className="text-center font-source-sans text-[24px] font-bold text-[#EBF2FA]">
         {des}
       </p>
