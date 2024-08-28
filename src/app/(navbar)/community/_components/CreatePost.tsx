07f9cc632510b790e728e658001123b4052a6efc
andi-frame <andifarhan1094@gmail.com>
2024-08-28 22:06:47 +0700

    feat: setup for community

---
+++ src/app/(navbar)/community/_components/CreatePost.tsx
@@ -0,0 +1,34 @@
+import { type Session } from "next-auth";
+import Image from "next/image";
+import React from "react";
+
+interface CreatePostProp {
+  session: Session;
+}
+
+const CreatePost: React.FC<CreatePostProp> = ({ session }) => {
+  return (
+    <div className="flex w-full">
+      <div className="flex w-1/12 items-start justify-center">
+        <Image
+          src={session.user.image ?? ""}
+          alt="user profile image"
+          width={48}
+          height={48}
+        />
+      </div>
+      <div className="w-9/12">
+        <textarea
+          name="postContent"
+          rows={5}
+          cols={33}
+          placeholder="What is happening?"
+          className="w-full"
+        ></textarea>
+      </div>
+      <div className="w-2/12"></div>
+    </div>
+  );
+};
+
+export default CreatePost;
