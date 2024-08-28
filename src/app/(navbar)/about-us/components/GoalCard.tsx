52f853f5c366e8a422b1e0e3bdcc32acdfc13f0f
andi-frame <andifarhan1094@gmail.com>
2024-08-28 20:07:39 +0700

    fix typesafety

--- src/app/(navbar)/about-us/components/GoalCard.tsx
+++ src/app/(navbar)/about-us/components/GoalCard.tsx
@@ -1,12 +1,21 @@
-const GoalCard = ({ link, des }) => (
-  <div className="flex flex-col w-1/2 border-2 bg-[#A5BE00] rounded-2xl border-[#679436] mx-6">
-    <div className="flex justify-center items-center my-2">
-      <img src={link} alt={des} className="w-24"/>
+import React from "react";
+
+interface GoalCardProps {
+  link: string;
+  des: string;
+}
+
+const GoalCard: React.FC<GoalCardProps> = ({ link, des }) => (
+  <div className="mx-6 flex w-1/2 flex-col rounded-2xl border-2 border-[#679436] bg-[#A5BE00]">
+    <div className="my-2 flex items-center justify-center">
+      <img src={link} alt={des} className="w-24" />
     </div>
     <div className="my-1">
-      <p className="text-[#EBF2FA] text-center font-source-sans font-bold text-[24px]">{des}</p>
+      <p className="text-center font-source-sans text-[24px] font-bold text-[#EBF2FA]">
+        {des}
+      </p>
     </div>
   </div>
-  );
+);
 
-export default GoalCard
+export default GoalCard;
--- src/app/(navbar)/about-us/components/OurTeam.tsx
+++ src/app/(navbar)/about-us/components/OurTeam.tsx
@@ -1,9 +1,23 @@
-const OurTeam = ({ foto, name, role }) => (
-  <div className="bg-[#679436] bg-opacity-[0.28] flex flex-col items-center w-72 h-auto border-gray-500 border-2 mx-2 my-2 rounded-2xl">
-    <img src={foto} alt={`${name} ${role}`} className="w-32 h-32 object-cover rounded-full my-4" />
-    <div className="w-11/12 flex flex-col bg-white text-[#679436] rounded-2xl mb-4 p-2">
-      <p className="font-montserrat whitespace-nowrap font-semibold text-[22px] text-center">{name}</p>
-      <p className="font-source-sans text-[16px] text-center">{role}</p>
+import React from "react";
+
+interface OurTeamProps {
+  foto: string;
+  name: string;
+  role: string;
+}
+
+const OurTeam: React.FC<OurTeamProps> = ({ foto, name, role }) => (
+  <div className="mx-2 my-2 flex h-auto w-72 flex-col items-center rounded-2xl border-2 border-gray-500 bg-[#679436] bg-opacity-[0.28]">
+    <img
+      src={foto}
+      alt={`${name} ${role}`}
+      className="my-4 h-32 w-32 rounded-full object-cover"
+    />
+    <div className="mb-4 flex w-11/12 flex-col rounded-2xl bg-white p-2 text-[#679436]">
+      <p className="whitespace-nowrap text-center font-montserrat text-[22px] font-semibold">
+        {name}
+      </p>
+      <p className="text-center font-source-sans text-[16px]">{role}</p>
     </div>
   </div>
 );
