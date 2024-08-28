db09955c52adb839ad3fbcb6782e020eb94344ea
andi-frame <andifarhan1094@gmail.com>
2024-08-28 19:23:36 +0700

    chore: remove unnecessary session fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,11 @@ import Navbar from "@/app/_components/Navbar";
 import { getServerAuthSession } from "@/server/auth";
 import { HydrateClient } from "@/trpc/server";
 import Logout from "./_components/logout";
-import { redirect } from "next/navigation";
 
 import TestUlploadImage from "./_components/testUploadImage";
 
 export default async function Home() {
   const session = await getServerAuthSession();
-  if (session?.user.role === "customer") {
-    redirect("/leftover");
-  }
-  if (session?.user.role === "merchant") {
-    redirect("/merchant");
-  }
 
   return (
     <HydrateClient>
