6e9e55ce7e115b6501b632b75a970f44ebef774c
andi-frame <andifarhan1094@gmail.com>
2024-09-08 19:58:10 +0700

    feat: update merchant info

--- src/app/merchant/_components/MerchantInfo.tsx
+++ src/app/merchant/_components/MerchantInfo.tsx
@@ -1,7 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
+import { type Session } from "next-auth";
+import { api } from "@/trpc/react";
 
-const MerchantInfo = () => {
+interface MerchantInfoProp {
+  session: Session;
+}
+
+const MerchantInfo: React.FC<MerchantInfoProp> = ({ session }) => {
+  const merchant = api.auth.getMerchantByUserId.useQuery(session.user.id);
+
   return (
     <div className="mt-32 flex h-24 w-10/12 items-center justify-between rounded-3xl bg-[#508bb5] px-8">
       <div className="flex gap-5">
@@ -14,15 +24,21 @@ const MerchantInfo = () => {
           ></Image>
         </div>
         <div className="flex flex-col justify-center gap-1">
-          <div className="text-xl font-bold text-[#EBF2FA]">Merchant Name</div>
-          <button className="rounded-lg bg-[#EBF2FA] px-3 text-center text-[#508bb5]">
+          <div className="text-xl font-bold text-[#EBF2FA]">
+            {merchant.data?.merchantName}
+          </div>
+          <button className="w-40 rounded-lg bg-[#EBF2FA] px-3 text-center text-[#508bb5]">
             Contact
           </button>
         </div>
       </div>
       <div className="flex flex-col items-end gap-1">
-        <div className="font-semibold text-[#EBF2FA]">Merchant Type</div>
-        <div className="font-semibold text-[#EBF2FA]">Merchant Location</div>
+        <div className="font-semibold text-[#EBF2FA]">
+          {merchant.data?.merchantType}
+        </div>
+        <div className="font-semibold text-[#EBF2FA]">
+          {merchant.data?.location}
+        </div>
       </div>
     </div>
   );
